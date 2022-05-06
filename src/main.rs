0a06237a85780ec0a86a2ff755b2a0681602dac7
Fernando Montero <fernando.montero.perez@gmail.com>
2022-05-06 16:56:06 +0100

    primitives

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
+    static age: u32 = 90;
+
+    let var = "Hello world";
+    let num = 32;
+    let num2 = 32u8;
+
+    // completeley different from JS's const
+    const NAME: &str = "CANARIASJS";
+    // at compile time usages of NAME get replaced by the value
+    // use const for values that can be determined at compile time
+
     println!("Hello, world!");
 }
